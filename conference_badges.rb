f4f1d78ddef062dffb91b7ae6c92ad03b11098a4
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-27 15:02:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+=begin
+def assign_rooms(array)
+  room_count = 0 
+  array.collect do |name|
+    room_count += 1 
+    "Hello, #{name}! You'll be assigned to room #{room_count}!"
+  end 
+end
+=end 
+
+def assign_rooms(array)
+  result = []
+  array.each_with_index do |name, index|
+    result << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  result 
+end
+
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+end 
